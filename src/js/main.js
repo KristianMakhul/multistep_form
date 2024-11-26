4dc8f7316e1cdd570be16c42633cffbbf5b4dc5c
Kris13561 <folseorno@gmail.com>
2024-11-27 01:22:03 +0200

    add validation

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,25 @@
+const form = document.querySelector(".form");
 const steps = document.querySelectorAll(".step");
 const nextBtn = document.querySelector(".next-btn");
 const prevBtn = document.querySelector(".prev-btn");
+const submitButton = document.querySelector(".submit-btn");
+const progressBar = document.querySelectorAll(".progress-bar");
+
 let currentStep = 0;
 
-const progressBar = document.querySelectorAll(".progress-bar");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  submitButton.textContent = "Submitting...";
+  submitButton.classList.add("cursor-not-allowed");
+  prevBtn.disabled = true;
+  prevBtn.classList.add("cursor-not-allowed");
+
+  setTimeout(() => {
+    alert("Form was submitted successfully");
+    location.reload();
+  }, 3000);
+});
 
 function updateProgressBar() {
   progressBar.forEach((step, index) => {
@@ -13,29 +29,46 @@ function updateProgressBar() {
       step.classList.remove("active");
     }
   });
- }
+}
 
 function showStep(index) {
   steps.forEach((step, i) => {
     step.classList.toggle("hidden", i !== index);
   });
 
-  if (currentStep === 0) {
-    prevBtn.classList.add("invisible");
-  } else {
-    prevBtn.classList.remove("invisible");
-  }
-  
-  if (currentStep === 3) {
-    nextBtn.classList.add("invisible");
-  } else {
-    nextBtn.classList.remove("invisible");
-  }
-  
+  prevBtn.classList.toggle("invisible", currentStep === 0);
+  nextBtn.classList.toggle("invisible", currentStep === 3);
+
   updateProgressBar();
 }
 
+const isValidStep = () => {
+  const fields = steps[currentStep].querySelectorAll("input");
+
+  const radioArray = [...fields].filter((field) => field.type === "radio");
+
+  if (radioArray.length > 0) {
+    const groupRadios = [...new Set(radioArray.map((radio) => radio.name))];
+
+    const isValid = groupRadios.every((elem) => {
+      const groupChecked = steps[currentStep].querySelector(
+        `input[name="${elem}"]:checked`,
+      );
+      return Boolean(groupChecked);
+    });
+
+    if (!isValid) {
+      alert("Please select one option.");
+      return false;
+    }
+  }
+
+  return [...fields].every((field) => field.reportValidity());
+};
+
 nextBtn.addEventListener("click", () => {
+  if (!isValidStep()) return;
+
   if (currentStep < steps.length - 1) {
     currentStep++;
     showStep(currentStep);
