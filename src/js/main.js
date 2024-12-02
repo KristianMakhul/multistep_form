8d65084c75b83ee005f98de9f12247d4244cdaa0
Kris13561 <folseorno@gmail.com>
2024-12-02 09:36:15 +0200

    improve js

--- src/js/main.js
+++ src/js/main.js
@@ -1,132 +1,145 @@
-const form = document.querySelector(".form");
-const steps = document.querySelectorAll(".step");
-const nextBtn = document.querySelector(".next-btn");
-const prevBtn = document.querySelector(".prev-btn");
-const submitButton = document.querySelector(".submit-btn");
-const progressBar = document.querySelectorAll(".progress-bar");
+class MultiStepForm {
+  constructor(formSelector, currentStep) {
+    this.form = document.querySelector(formSelector);
+    this.steps = document.querySelectorAll(".step");
+    this.nextBtn = document.querySelector(".next-btn");
+    this.prevBtn = document.querySelector(".prev-btn");
+    this.submitButton = document.querySelector(".submit-btn");
+    this.progressBar = document.querySelectorAll(".progress-bar");
+    this.currentStep = currentStep;
 
-let currentStep = 0;
+    this.validationRules = {
+      name: {
+        required: true,
+        maxlength: 15,
+        message: "Name is required and must be less than 15 characters.",
+      },
+      email: {
+        required: true,
+        pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        message: "Please enter a valid email address.(example@gmail.com)",
+      },
+      tel: {
+        required: true,
+        minlength: 6,
+        maxlength: 12,
+        pattern: /^[0-9]*$/,
+        message: "Phone number must be between 6 and 12 digits.",
+      },
+      company: {
+        required: true,
+        maxlength: 15,
+        message:
+          "Company name is required and must be less than 15 characters.",
+      },
+    };
 
-const validationRules = {
-  name: {
-    required: true,
-    maxlength: 15,
-    message: "Name is required and must be less than 15 characters.",
-  },
-  email: {
-    required: true,
-    pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-    message: "Please enter a valid email address.(example@gmail.com)",
-  },
-  tel: {
-    required: true,
-    minlength: 6,
-    maxlength: 12,
-    pattern: /^[0-9]*$/,
-    message: "Phone number must be between 6 and 12 digits.",
-  },
-  company: {
-    required: true,
-    maxlength: 15,
-    message: "Company name is required and must be less than 15 characters.",
-  },
-};
-
-const validateField = (field, rules) => {
-  const value = field.value.trim();
-  const { required, maxlength, minlength, pattern, message } = rules;
-
-  const hasError = [
-    required && !value,
-    maxlength && value.length > maxlength,
-    minlength && value.length < minlength,
-    pattern && !pattern.test(value),
-  ].includes(true);
-  hasError && alert(message);
-  return !hasError;
-};
-
-const validateRadioButtons = (fields) => {
-  const radioArray = [...fields].filter((field) => field.type === "radio");
-
-  if (radioArray.length > 0) {
-    const [groupName] = [...new Set(radioArray.map((radio) => radio.name))];
-
-    const checkedRadio = steps[currentStep].querySelector(
-      `input[name="${groupName}"]:checked`,
-    );
-
-    if (!checkedRadio) {
-      alert("Please select an option.");
-      return false;
-    }
+    this.init(this.currentStep);
   }
 
-  return true;
-};
+  init(curStep) {
+    this.showStep(curStep);
+    this.nextBtn.addEventListener("click", this.handleNext.bind(this));
+    this.prevBtn.addEventListener("click", this.handlePrev.bind(this));
+    this.form.addEventListener("submit", this.handleSubmit.bind(this));
+  }
 
-const isValidStep = () => {
-  const fields = steps[currentStep].querySelectorAll("input");
+  validateField(field, rules) {
+    const value = field.value.trim();
+    const { required, maxlength, minlength, pattern, message } = rules;
 
-  for (const field of fields) {
-    if (field.type !== "radio") {
-      const rules = validationRules[field.id];
-      if (!validateField(field, rules)) {
+    const hasError = [
+      required && !value,
+      maxlength && value.length > maxlength,
+      minlength && value.length < minlength,
+      pattern && !pattern.test(value),
+    ].includes(true);
+    hasError && alert(message);
+    return !hasError;
+  }
+
+  validateRadioButtons(fields) {
+    const radioArray = [...fields].filter((field) => field.type === "radio");
+
+    if (radioArray.length > 0) {
+      const [groupName] = [...new Set(radioArray.map((radio) => radio.name))];
+
+      const checkedRadio = this.steps[this.currentStep].querySelector(
+        `input[name="${groupName}"]:checked`,
+      );
+
+      if (!checkedRadio) {
+        alert("Please select an option.");
         return false;
       }
     }
+
+    return true;
   }
 
-  return validateRadioButtons(fields);
-};
+  isValidStep() {
+    const fields = this.steps[this.currentStep].querySelectorAll("input");
 
-function updateProgressBar() {
-  progressBar.forEach((step, index) => {
-    step.classList.toggle("active", index <= currentStep);
-  });
+    for (const field of fields) {
+      if (field.type !== "radio") {
+        const rules = this.validationRules[field.id];
+        if (!this.validateField(field, rules)) {
+          return false;
+        }
+      }
+    }
+
+    return this.validateRadioButtons(fields);
+  }
+
+  updateProgressBar() {
+    this.progressBar.forEach((step, index) => {
+      step.classList.toggle("active", index <= this.currentStep);
+    });
+  }
+
+  showStep(index) {
+    this.steps.forEach((step, i) => {
+      step.classList.toggle("hidden", i !== index);
+    });
+
+    this.prevBtn.classList.toggle("invisible", this.currentStep === 0);
+    this.nextBtn.classList.toggle("invisible", this.currentStep === 3);
+
+    this.updateProgressBar();
+  }
+
+  handleNext() {
+    if (!this.isValidStep()) return;
+
+    if (this.currentStep < this.steps.length - 1) {
+      this.currentStep++;
+      this.showStep(this.currentStep);
+    }
+  }
+
+  handlePrev() {
+    if (this.currentStep > 0) {
+      this.currentStep--;
+      this.showStep(this.currentStep);
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    if (!this.isValidStep()) return;
+
+    this.submitButton.textContent = "Submitting...";
+    this.submitButton.classList.add("cursor-not-allowed");
+    this.prevBtn.disabled = true;
+    this.prevBtn.classList.add("cursor-not-allowed");
+
+    setTimeout(() => {
+      alert("Form was submitted successfully");
+      location.reload();
+    }, 3000);
+  }
 }
 
-function showStep(index) {
-  steps.forEach((step, i) => {
-    step.classList.toggle("hidden", i !== index);
-  });
-
-  prevBtn.classList.toggle("invisible", currentStep === 0);
-  nextBtn.classList.toggle("invisible", currentStep === 3);
-
-  updateProgressBar();
-}
-
-nextBtn.addEventListener("click", () => {
-  if (!isValidStep()) return;
-
-  if (currentStep < steps.length - 1) {
-    currentStep++;
-    showStep(currentStep);
-  }
-});
-
-prevBtn.addEventListener("click", () => {
-  if (currentStep > 0) {
-    currentStep--;
-    showStep(currentStep);
-  }
-});
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  if (!isValidStep()) return;
-
-  submitButton.textContent = "Submitting...";
-  submitButton.classList.add("cursor-not-allowed");
-  prevBtn.disabled = true;
-  prevBtn.classList.add("cursor-not-allowed");
-
-  setTimeout(() => {
-    alert("Form was submitted successfully");
-    location.reload();
-  }, 3000);
-});
-
-showStep(currentStep);
+new MultiStepForm(".form", 0);
